5032a0d60c0910070c022dd80e1f778447b9ea17
sof <sofiene.belharbi@gmail.com>
2024-09-03 09:41:38 +0200

    Fist commit

---
+++ terraform-manifests/c4-random-resources.tf
@@ -0,0 +1,6 @@
+# Random String Resource
+resource "random_string" "myrandom" {
+  length = 6
+  upper = false 
+  special = false  
+}
---
+++ terraform-manifests/terraform.tfvars
@@ -0,0 +1,4 @@
+# Generic Variables 
+business_divsion = "sbh"
+resource_group_location = "france central"
+resource_group_name = "rg"
